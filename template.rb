648b4b9c1d31a264be96ef34e566d05cbb2b2607
Orlando Quero <orlaqp@gmail.com>
2014-02-02 13:14:48 -0500

    Included a new template for the scaffold_controller

--- template.rb
+++ template.rb
@@ -50,6 +50,84 @@ file 'app/assets/javascripts/templates/application.handlebars', <<-CODE
 </div>
 CODE
 
+# modify scaffold_controller generator 
+create_file 'lib/templates/rails/scaffold_controller/controller.rb', <<-CODE
+
+<% if namespaced? -%>
+require_dependency "<%= namespaced_file_path %>/application_controller"
+
+<% end -%>
+<% module_namespacing do -%>
+class <%= controller_class_name %>Controller < ApplicationController
+  respond_to :json
+
+  before_action :set_<%= singular_table_name %>, only: [:show, :edit, :update, :destroy]
+
+  # GET <%= route_url %>
+  def index
+    respond_with @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
+  end
+
+  # GET <%= route_url %>/1
+  def show
+    respond_with @<%= singular_table_name %>
+  end
+
+  # GET <%= route_url %>/new
+  def new
+    respond_with @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
+  end
+
+  # GET <%= route_url %>/1/edit
+  def edit
+    respond_with @<%= singular_table_name %>
+  end
+
+  # POST <%= route_url %>
+  def create
+    @<%= singular_table_name %> = <%= orm_class.build(class_name, singular_table_name + "_params") %>
+
+    if @<%= orm_instance.save %>
+      render action: 'show', status: :created, location: @<%= singular_table_name %> 
+    else
+      render json: { errors: @<%= singular_table_name %>.errors }, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT <%= route_url %>/1
+  def update
+    if @<%= orm_instance.update(singular_table_name + "_params") %>
+      head :no_content
+    else
+      render json: { errors: @<%= singular_table_name %>.errors }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE <%= route_url %>/1
+  def destroy
+    @<%= orm_instance.destroy %>
+    head :no_content 
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_<%= singular_table_name %>
+      @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def <%= singular_table_name + "_params" %>
+      <%- if attributes_names.empty? -%>
+      params[<%= ":" + singular_table_name %>]
+      <%- else -%>
+      params.require(<%= ":" + singular_table_name %>).permit(<%= attributes_names.map { |name| ":" + name }.join(', ') %>)
+      <%- end -%>
+    end
+end
+<% end -%>
+
+CODE
+
 run "rm -rf app/views/layouts"
 route "root :to => 'assets#index'"
 
@@ -67,10 +145,22 @@ remove_file 'public/index.html'
 app_name = ask('Ember Application Name: (default: App)')
 app_name = "App" unless not app_name.blank?
 
-application "# Sets ember's application name\n  config.ember.app_name='#{app_name}'"
+application "# Sets ember's application name\n     config.ember.app_name='#{app_name}'"
+
+application <<-CODE
+    # customize scaffold generator to aoivd the creation of unnecesary code
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine false
+      g.jbuilder        false
+      g.helper          false
+      g.test_framework  false
+      g.stylesheets     false
+    end
+CODE
 
 # generate ember bootstrap with support for ember script
-generate "ember:bootstrap -g --javascript-engine=em"
+generate "ember:bootstrap --javascript-engine=em"
 
 # I need to inject jquery into application.js.em because it is not included by default by ember bootstrap geenerator
 inject_into_file "app/assets/javascripts/application.js.em", "#= require jquery\n", :before => "#= require handlebars\n"
